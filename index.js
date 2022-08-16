31554ef49d50398b336ce0ba986717ebda48151d
Brenda <waiyegobrenda99@gmail.com>
2022-08-17 01:27:19 +0300

    complted assignment

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(sample){
+  const feet = 199
+if (sample <=400) {
+  return (`This one is on me!`)
+}
+else if (sample>2000 && sample <2500){
+  return (`I will gladly take your thirty bucks.`)
+}
+else if (sample <= 2501){
+  return (`No can do.`)
+}
+}
+function ternaryCheckCity(city){
+  if (city === 'NYC'){
+  return "Ok, sounds good." ;
+  }
+ else(city === 'Pittsburgh')
+  return "No go.";
+ 
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch (tip){
+    case 'generous':
+    return "Thank you so much.";
+    break;
+    case "not as generous":
+    return "Thank you.";
+    break;
+    default:
+      return "Bye.";
+  }
 }
